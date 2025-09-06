1d7a7236a04ed974605c019a6f23e84047000230
neon443 <69979447+neon443@users.noreply.github.com>
2025-09-06 18:32:32 +0100

    Initial Commit

---
+++ ProximiChat/ProximiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProximiChatApp.swift
+//  ProximiChat
+//
+//  Created by neon443 on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProximiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
